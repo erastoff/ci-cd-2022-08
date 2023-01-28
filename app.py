67cbadef14ea9861243fd1ed0b654fe856720774
erastoff <yuriy.erastov@yandex.ru>
2023-01-28 09:11:10 +0300

    first commit - Flask app files added

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, render_template
+from views.products import products_app  # аналог подключения роутера в FastAPI
+
+app = Flask(__name__) # создание приложения (класса от Flask)
+
+app.config.update(
+    ENV="development",
+    SECRET_KEY="fgdfgfdbdfgbdfgghyhrrwe"
+)
+
+app.register_blueprint(products_app, url_prefix="/products")
+
+@app.route("/")  # обычная ввью функция, которая выведет дынные
+def hello_world():
+    print_request()
+    # return "<h1>Hello, World!<h1>"
+    return render_template("index.html")
+    # return render_template("base.html")
+
+def print_request():
+    # различные свойства данного запроса
+    print("request", request)
+    print("request.method", request.method)
+    print("request.path", request.path)
+
+
+@app.route("/hello/")  # "/hello/" и "/hello" - два разных маршрута
+@app.route("/hello/<name>/")  # можем накрутить два декоратора (два маршрута)
+def hello_view(name: str = None):
+    print_request()
+    if name is None:
+        name = request.args.get("name", "")  # если передано несколько имен - берем первое в запросе
+    name = name.strip()
+    if not name:
+        name = "world"
+
+    # names = request.args.getlist("name")  # поддержка нескольких ключей
+    # print(request.args)
+
+    # print("request", request)  # переменная request доступна только внутри вью-функции
+    # (нельзя обратиться к "запросу вне запроса")
+
+    return {"message": f"Hello {name}"}  # Flask понимает, что это json и возвращает его
+
+
+@app.route("/items/<int:item_id>/")
+def get_item(item_id: int):
+    return {
+        "item": {"id": item_id},
+    }
+
+
+@app.route("/items/<item_id>/")
+def get_item_string(item_id: str):
+    return {
+        "item_id": {"id": item_id.upper()},
+    }
+
+
+# @app.route("/hello/<name>/")
+# def hello_name(name: str):
+#     return {"message": f"Hello {name}"}
+
+#NEVER
+# print("request", request)  # (нельзя обратиться к "запросу вне запроса")
+if __name__ == "__main__":
+    # app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
+# это первый тип запуска приложения
+# второй - через терминал; flask --app main run
---
+++ views/products.py
@@ -0,0 +1,57 @@
+# from main import app  - циклический импорт
+from flask import Blueprint, render_template, request, url_for, redirect
+from werkzeug.exceptions import BadRequest, NotFound
+
+from views.forms.products import CreateProductForm
+
+# print("__name__", __name__)
+products_app = Blueprint("products_app", __name__)  # аналог APIRouter в FastAPI
+
+PRODUCTS = {
+    1: "Laptop",
+    2: "Smartphone",
+    3: "Tablet",
+}
+
+@products_app.route("/", endpoint="list")
+def get_products():
+    return render_template("products/list.html", products=PRODUCTS)  # рендер темплита и передача продуктов в шаблон
+
+
+@products_app.route("/<int:product_id>/", endpoint="details")
+def get_product(product_id: int):
+    product_name = PRODUCTS.get(product_id)
+    if product_name is None:
+        raise NotFound(f"Product {product_id} not found")
+    return render_template(
+        "/products/details.html",
+        product_id=product_id,
+        product_name=product_name,
+    )
+
+
+
+@products_app.route("/add/", methods=["GET", "POST"], endpoint="add")
+def add_product():
+    form = CreateProductForm()
+
+    if request.method == "GET":
+        return render_template("products/add.html", form=form)
+
+    if not form.validate_on_submit():
+        return render_template("products/add.html", form=form), 400
+    print(request.form)
+    # product_name = request.form.get("product-name", "")
+    # product_name = product_name.strip()
+    # if not product_name:
+    #     raise BadRequest("Field 'product-name' is required")
+
+    product_name = form.name.data  # вынесли часть логики в форму (закоментированный блок выше - аналогично, но сложнее)
+
+    product_id = len(PRODUCTS) + 1
+    PRODUCTS[product_id] = product_name
+
+    url = url_for("products_app.details", product_id=product_id)
+    return redirect(url)
+
+
