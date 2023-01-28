384623e351c3c21a7c17855db7372893c7791bf8
erastoff <yuriy.erastov@yandex.ru>
2023-01-28 14:52:46 +0300

    Tests updated

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,9 +1,17 @@
+"""
+Test configuration
+"""
+
 import pytest
 from flask.testing import FlaskClient
 from app import app
 
 @pytest.fixture
 def client() -> FlaskClient:
+    """
+    client creation
+    :return:
+    """
     with app.test_client() as test_client:
         with app.app_context():
             yield test_client
